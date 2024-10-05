a1e11c07f69c76f7a775100eaa1879c571b5feaf
Kirtan-lokadiya <kirtanlokadiya998@gmail.com>
2024-10-05 17:10:51 +0530

    change in deploy.yml file (build)

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "mern-backend",
       script: "./app.js", // Path to your main app file
       env: {
-        PORT: 5000,
+        PORT: 5001,
         MONGO_URI: process.env.MONGO_URI, // This loads your MongoDB URI from .env
       },
     },
